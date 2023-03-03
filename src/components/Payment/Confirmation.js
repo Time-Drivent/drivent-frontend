66a459720a6c1c9319e2b313557707a2337cb313
Lucas F. Silva <lucasdotfs@gmail.com>
2023-03-03 16:33:24 -0300

    refactor: delete unnecessary files

--- src/components/Payment/Confirmation.js
+++ src/components/Payment/Confirmation.js
@@ -5,24 +5,20 @@ import styled from 'styled-components';
 export function Confirmation({ price }) {
   return (
     <ConfirmationContainer>
-      <div>
-        Fechado! O total ficou em R$ {price}. Agora é só confirmar:
-      </div>
+      <div>Fechado! O total ficou em R$ {price}. Agora é só confirmar:</div>
 
-      <Button type="submit">
-        RESERVAR INGRESSO
-      </Button>
+      <Button type="submit">RESERVAR INGRESSO</Button>
     </ConfirmationContainer>
   );
 }
 
 const ConfirmationContainer = styled.div`
-  color: #8E8E8E;
+  color: #8e8e8e;
   font-family: 'Roboto';
   font-size: 20px;
   line-height: 23px;
   margin-top: 40px;
-  
+
   div {
     margin-bottom: 10px;
   }
