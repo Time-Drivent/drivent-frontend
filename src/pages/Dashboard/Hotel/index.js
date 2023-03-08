00c4d8941cbc16f4ef3d55bd522b4004465c6b8c
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2023-03-08 06:06:39 -0300

    feat: no payment or no hotel text in page

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import MessageContainer from '../../../components/MessageContainer';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const messageContainerPhrases = {
+    noPayment: ['Você precisa ter confirmado pagamento antes', 'de fazer a escolha de hospedagem'],
+    noHotel: ['Sua modalidade de ingresso não inclui hospedagem', 'Prossiga para a escolha de atividades'],
+    noTicket: ['Você precisa ter criado e pago seu ticket antes', 'de fazer a escolha de hospedagem'],
+  };
+  const [ticket, setTicket] = useState({});
+  const { getTicket, getTicketLoading } = useTicket();
+  // eslint-disable-next-line space-before-function-paren
+  useEffect(async () => {
+    try {
+      const ticket = await getTicket();
+      setTicket(ticket);
+    } catch (error) {
+      if (error.response.status === 404) setTicket({ noTicket: true });
+      toast('Não foi possível encontrar as informações do seu Ticket');
+    }
+  }, []);
+  return (
+    <>
+      {ticket.noTicket ? (
+        <MessageContainer phrases={messageContainerPhrases.noTicket} />
+      ) : ticket.status === 'PAID' ? (
+        ticket.TicketType.includesHotel ? (
+          <></>
+        ) : (
+          <MessageContainer phrases={messageContainerPhrases.noHotel} />
+        )
+      ) : (
+        <MessageContainer phrases={messageContainerPhrases.noPayment} />
+      )}
+    </>
+  );
 }
