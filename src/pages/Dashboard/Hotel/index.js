86348ce84415625c4137e0551792740d08945ad7
Lucas F. Silva <lucasdotfs@gmail.com>
2023-03-08 12:54:44 -0300

    feat: add hotels menu

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,18 @@
+import { Typography } from '@material-ui/core';
+import { useState } from 'react';
+import styled from 'styled-components';
+import HotelInformation from '../../../components/HotelInformation';
+import MessageContainer from '../../../components/MessageContainer';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const [accessDenied, setAccessDenied] = useState(false);
+
+  return <>
+    <StyledTypography variant='h4'>Escolha de hotel e quarto</StyledTypography> 
+    { accessDenied ? <MessageContainer phrases={['Problema']} /> : <HotelInformation /> }
+  </>;
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
