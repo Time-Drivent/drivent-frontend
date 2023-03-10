794ee9b7b5b2228cf3bf26345daf598e1fa5d8cc
lcfernandez <lcfernandez@cos.ufrj.br>
2023-03-10 10:15:35 -0300

    refactor: change setBooking logic

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,14 +1,16 @@
 import { Typography } from '@material-ui/core';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import HotelInformation from '../../../components/HotelInformation';
 import MessageContainer from '../../../components/MessageContainer';
 import { toast } from 'react-toastify';
 import useTicket from '../../../hooks/api/useTicket';
 import HotelConfirm from '../../../components/HotelConfirm';
+import UserContext from '../../../contexts/UserContext';
+import { getUserBooking } from '../../../services/bookingApi';
 
 export default function Hotel() {
-  const [booking, setbooking] = useState({});
+  const [booking, setBooking] = useState(undefined);
   const messageContainerPhrases = {
     noPayment: ['Você precisa ter confirmado pagamento antes', 'de fazer a escolha de hospedagem'],
     noHotel: ['Sua modalidade de ingresso não inclui hospedagem', 'Prossiga para a escolha de atividades'],
@@ -16,6 +18,15 @@ export default function Hotel() {
   };
   const [ticket, setTicket] = useState({});
   const { getTicket, getTicketLoading } = useTicket();
+  const { userData } = useContext(UserContext);
+  const token = userData.token;
+  
+  useEffect(() => {
+    getUserBooking(token).then((res) => {
+      setBooking(res);
+    });
+  }, []);
+
   // eslint-disable-next-line space-before-function-paren
   useEffect(async () => {
     try {
@@ -26,6 +37,7 @@ export default function Hotel() {
       toast('Não foi possível encontrar as informações do seu Ticket');
     }
   }, []);
+
   return (
     <>
       <StyledTypography variant='h4'>Escolha de hotel e quarto</StyledTypography>
@@ -41,7 +53,7 @@ export default function Hotel() {
         <MessageContainer phrases={messageContainerPhrases.noPayment} />
       )}
       {!booking ? <HotelInformation /> :
-        <HotelConfirm booking={booking} setbooking={setbooking} />
+        <HotelConfirm booking={booking} />
       }
     </>
   );
