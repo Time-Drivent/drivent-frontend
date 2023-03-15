315d5e5be33680fa98cf5206ec815e5765517396
Lucas F. Silva <lucasdotfs@gmail.com>
2023-03-15 11:16:11 -0300

    refactor: move day filter from pages/Activities to components/ActivitiesInformation

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -8,10 +8,7 @@ import styled from 'styled-components';
 
 export default function Activities() {
   const { getTicket, getTicketLoading } = useTicket();
-  const { getDay, getDayLoading } = useDay();
   const [Component, setComponent] = useState(null);
-  const [days, setDays] = useState([]);
-  const [selectedDayId, setSelectedDayId] = useState();
 
   // eslint-disable-next-line space-before-function-paren
   useEffect(async () => {
@@ -32,36 +29,15 @@ export default function Activities() {
       }
 
       setComponent(() => <ActivitiesInformation />);
-      const daysAux = await getDay();
-      setDays([...daysAux]);
     } catch (error) {
       console.log(error);
     }
   }, []);
 
-  function handleDaySelection(id) {
-    setSelectedDayId(id);
-  }
-
   return (
     <>
       <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
-      <Title>Primeiro, filtre pelo dia do evento:</Title>
       {!getTicketLoading ? Component : null}
-      <DaysList>
-        {days.map((day) => {
-          return (
-            <Day key={day.date} selectedDayId={selectedDayId} dayId={day.id} onClick={() => handleDaySelection(day.id)}>
-              <h1>
-                {`${day.weekday}, ${new Date(day.date).toLocaleDateString('pt-br', {
-                  day: 'numeric',
-                  month: 'numeric',
-                })}`}
-              </h1>
-            </Day>
-          );
-        })}
-      </DaysList>
     </>
   );
 }
@@ -70,38 +46,3 @@ const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
 `;
 
-const DaysList = styled.div`
-  display: flex;
-  gap: 18px;
-`;
-
-const Day = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 131px;
-  height: 37px;
-  background: ${(props) => (props.selectedDayId === props.dayId ? '#FFD37D' : '#e0e0e0')};
-  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
-  border-radius: 4px;
-  h1 {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    color: #000;
-  }
-  :hover {
-    cursor: pointer;
-  }
-`;
-
-const Title = styled.h1`
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  color: #8e8e8e;
-  margin-bottom: 22px;
-  margin-top: 36px;
-`;
